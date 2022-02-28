d5aa955d7a8c3d25d3d1ada38c621414ff944471
Vaishnavi170 <84765083+Vaishnavi170@users.noreply.github.com>
2022-02-28 16:01:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -17,14 +17,14 @@ function setup(){
 function draw(){
     image(video,0,0,380,380);
     if(status != ""){
-        objectDetector.detect (video,gotResults);
+        objectDetector.detect (video,gotResult);
         for(i = 0; i<objects.length;i++){
             document.getElementById("status").innerHTML = "Objects detected";
             document.getElementById("number_of_objects").innerHTML = "number of objects detected are : " + objects.length;
         
             fill("#FF0000");
             percent = floor(objects[i].confidence * 100);
-            text(objects[i].label + " " + percent + "%",object[i].x +15,object[i].y + 15);
+            text(objects[i].label + " " + percent + "%",objects[i].x +15,objects[i].y + 15);
             noFill();
             stroke("#FF0000");
             rect(objects[i].x,objects[i].y,objects[i].width, objects[i].height);
@@ -32,13 +32,7 @@ function draw(){
     }
    }
 
-function gotResult(error,results) {
-    if (error) {
-        console.log(error);
-    }
-    console.log(results);
-    objects = results;
-}
+
 
 function start() {
     objectDetector = ml5.objectDetector('cocossd',modelLoaded);
@@ -49,4 +43,11 @@ function modelLoaded() {
     console.log("Model Loaded!");
     status = true;
     objectDetector.detect(video,gotResult);
+}
+function gotResult(error,results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
